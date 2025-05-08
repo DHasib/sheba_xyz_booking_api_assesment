741827b6a7ac480d5d6fb68e7b2e045427138805
dhasib <mdhasib522@gmail.com>
2025-05-08 10:31:53 +0600

    feat: implement API middleware for JSON requests and create personal access tokens migration using laravel Sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+use App\Http\Middleware\ForceJsonRequestHeader;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(ForceJsonRequestHeader::class);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request) {
+            return $request->is('api/*');
+        });
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'message' => 'Not Authorized To Access This Resource. please login to continue',
+            ], 401);
+        });
     })->create();
