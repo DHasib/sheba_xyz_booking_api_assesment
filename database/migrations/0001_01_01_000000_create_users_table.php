fcdeb7c18eb8fc6fd7d4c571b944c5719e079efe
dhasib <mdhasib522@gmail.com>
2025-05-08 21:52:14 +0600

    feat: add migrations for roles, categories, discounts, services, service_employee, and bookings tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('phone')->unique();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete()->index();
+            
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_05_08_151700_create_discounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('type',['percentage','fixed']);
+            $table->decimal('value',8,2);
+            $table->date('start_date')->index();
+            $table->date('end_date')->index();
+            $table->timestamps();
+
+            $table->index(['start_date','end_date']);
+            $table->index(['service_id','start_date','end_date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
