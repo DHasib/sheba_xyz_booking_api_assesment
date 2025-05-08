10ae796b6b579bb5a5ca5f3afa073d3d0958a7b5
dhasib <mdhasib522@gmail.com>
2025-05-08 19:25:53 +0600

    Remove commented-out code and return a structured JSON response in the API route.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //return json response(['message' => 'Hello World']);
 
     return [
         'message' => 'Hello World',
